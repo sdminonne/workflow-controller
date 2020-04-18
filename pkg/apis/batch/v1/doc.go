acd1794047de2c4115fb85ebb5d3b1fbfd0b9f56
Salvatore Dario Minonne <salvatore-dario.minonne@amadeus.com>
2020-04-18 22:25:52 +0200

    Add API: operator-sdk add api --api-version=batch.amadeus.io/v1 --kind=Workflow

---
+++ pkg/apis/batch/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the batch v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=batch.amadeus.io
+package v1
---
+++ pkg/apis/batch/v1/workflow_types.go
@@ -0,0 +1,48 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// WorkflowSpec defines the desired state of Workflow
+type WorkflowSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// WorkflowStatus defines the observed state of Workflow
+type WorkflowStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Workflow is the Schema for the workflows API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=workflows,scope=Namespaced
+type Workflow struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   WorkflowSpec   `json:"spec,omitempty"`
+	Status WorkflowStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// WorkflowList contains a list of Workflow
+type WorkflowList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Workflow `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Workflow{}, &WorkflowList{})
+}
